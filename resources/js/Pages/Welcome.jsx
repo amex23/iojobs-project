7bba3aba3e8bc9505e6b516ceeb1b495af601630
jantinrx23 <Janamir@tinrx.com>
2026-05-01 14:35:10 +0800

    welcome update

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -75,7 +75,7 @@ export default function Welcome({ auth, laravelVersion, phpVersion, featuredJobs
                                         <div className='flex flex-col gap-y-5 lg:gap-y-28'>
                                             <h1 className="pd text-black text-3xl xl:text-5xl">
                                                 Hire the best
-                                                Remote Professionals/Workers in the Philippines
+                                                Remote & Local Professionals/Workers in the Philippines
                                             </h1>
                                             <span className='hidden lg:flex w-full justify-between lg:bg-[#141413] text-white px-4 py-3 rounded-lg lg:flex-ro'>
                                                 <span className='flex flex-row text-2xl font-bold'>• Talent</span>
